b3cf43c4f33dca14bbaf80a02b9076b3506d810f
Smug <142998665+Smugpng@users.noreply.github.com>
2024-11-20 15:16:57 -0500

    This works for now! image check

--- Assets/Scripts/Image Manager.cs
+++ Assets/Scripts/Image Manager.cs
@@ -8,36 +8,47 @@ using Random = UnityEngine.Random;
 
 public class ImageManager : MonoBehaviour
 {
-    public RawImage image;
-    private const string webImage = "https://upload.wikimedia.org/wikipedia/commons/6/6a/The_birthday_cake_LCCN2013648266.jpg";
-    private const string webImage1 = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/A_cat_cleaning_its_claws_LCCN2009631922.tif/lossy-page1-800px-A_cat_cleaning_its_claws_LCCN2009631922.tif.jpg?20180404072544";
-    private const string webImage2 = "https://commons.wikimedia.org/wiki/File:Southern_Hairy-nosed_Wombat.jpg#/media/File:Southern_Hairy-nosed_Wombat.jpg";
+    public RawImage[] image;
+    
+
+    public string[] webpages;
+    private int webCount;
+
+    public Dictionary<string, Texture2D> savedImages;
+    int numb;
     public void Start()
     {
-        int rnd = Random.Range(1, 3);
-        switch (rnd)
+        savedImages = new Dictionary<string, Texture2D>();
+        savedImages.Clear();
+        for (int i = 0; i < image.Length; i++) 
         {
-            case 1:
-                DownloadImage(AddImage,webImage);
-                break;
-            case 2:
-                DownloadImage(AddImage,webImage1);
-                break;
-            case 3:
-                DownloadImage(AddImage, webImage2);
-                break;
+            webCount = Random.Range(0, 3);
+            GetWebImage(AddImage);            
         }
     }
-    public IEnumerator DownloadImage(Action<Texture2D> callback, string url)
+    public IEnumerator DownloadImage(Action<Texture2D> callback)
     {
-        UnityWebRequest request = UnityWebRequestTexture.GetTexture(url);
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(webpages[webCount]);
         yield return request.SendWebRequest();
         callback(DownloadHandlerTexture.GetContent(request));
-
+        savedImages.Add(webpages[webCount], DownloadHandlerTexture.GetContent(request));
     }
+    
+    public void GetWebImage(Action<Texture2D> callback)
+    {
+        if (savedImages.ContainsKey(webpages[webCount]))
+        {
+            callback(savedImages[webpages[webCount]]);           
+        }
+        else
+        {
+            StartCoroutine(DownloadImage(AddImage));
+        }
+    }
+
     public void AddImage(Texture2D texture)
     {
-        Debug.Log("what");
-        image.texture = texture;
+        numb++;
+        image[numb - 1].texture = texture;
     }
 }
